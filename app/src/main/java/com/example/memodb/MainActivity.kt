4c4385ddc87b31f77a0f13195561719d4e9601c3
MinWoo Lee <rkdxhd97@naver.com>
2022-11-29 18:37:29 +0900

    first commit

--- app/src/main/java/com/example/memodb/MainActivity.kt
+++ app/src/main/java/com/example/memodb/MainActivity.kt
@@ -26,16 +26,34 @@ class MainActivity : AppCompatActivity() {
 
         val dataRVAdapter = DataRVAdapter(dataList)
 
+        val roomDB = AppDataBase.getInstance(this)
+        if(roomDB != null) {
+            // 앱을 시작하면 roomDB에 값이 있는 경우 그 데이터를 가지고 온다.
+            val memoList = roomDB.memoDao().selectAll()
+
+            for(i in 0 until memoList.size)
+                dataList.add(memoList[i]) // 가지고 온 데이터를 recycler view에 하나씩 넣어준다.
+
+            viewBinding.rvData.adapter = dataRVAdapter
+            viewBinding.rvData.layoutManager = LinearLayoutManager(this)
+        }
         // registerForActivityResult
         // 메모장에 입력되는 data를 받아서 메모장의 결과를 저장하는 변수를 생성
         getResultText = registerForActivityResult (
             ActivityResultContracts.StartActivityForResult()) { result ->
             if (result.resultCode == RESULT_OK){
-                val content = result.data?.getStringExtra("text").toString()
-                val title = result.data?.getStringExtra("title").toString()
-                dataList.add(MemoData(content, title))
-                viewBinding.rvData.adapter = dataRVAdapter
-                viewBinding.rvData.layoutManager = LinearLayoutManager(this)
+                if(roomDB != null) {
+                    val memoList = roomDB.memoDao().selectAll()
+                    dataList.add(memoList[memoList.size-1])
+
+                    viewBinding.rvData.adapter = dataRVAdapter
+                    viewBinding.rvData.layoutManager = LinearLayoutManager(this)
+                }
+//                val content = result.data?.getStringExtra("text").toString()
+//                val title = result.data?.getStringExtra("title").toString()
+//                dataList.add(MemoData(content, title))
+//                viewBinding.rvData.adapter = dataRVAdapter
+//                viewBinding.rvData.layoutManager = LinearLayoutManager(this)
             }
         }
 
@@ -47,11 +65,13 @@ class MainActivity : AppCompatActivity() {
 
         dataRVAdapter.setItemClickListener(object: DataRVAdapter.onItemClickListener {
             override fun onClick(v: View, position: Int) {
-                var intent = Intent(this, SecondActivity::class.java)
-                startActivity(intent)
-                // Toast.makeText(viewBinding.root.context, "삭제", Toast.LENGTH_SHORT).show()
-                // dataList.removeAt(position)
-                // dataRVAdapter.notifyDataSetChanged()
+                Toast.makeText(viewBinding.root.context, "삭제", Toast.LENGTH_SHORT).show()
+                dataList.removeAt(position)
+                dataRVAdapter.notifyDataSetChanged()
+
+                val roomDB = AppDataBase.getInstance(this@MainActivity)
+                roomDB?.memoDao()?.delete(dataList[position])
+                dataRVAdapter.notifyDataSetChanged()
             }
         })
     }
